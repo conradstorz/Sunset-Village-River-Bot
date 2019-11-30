154c1597a9eaf44bb0b750b65083a24113ecb999
Conrad Storz <conradstorz@gmail.com>
2019-11-30 15:00:47 -0500

    Initial state of working code.

--- Sunset Village TwitterBot.py
+++ Sunset Village TwitterBot.py
@@ -3,7 +3,7 @@
 
 """ This is a twitterBot that will periodically tweet the projected river level at Bushman's Lake based on the 
 NWS website data for the river level both upstream and downstream then calculating the slope of the river
-to get the calculated level at our property.
+to get the calculated level at our property. 
 """
 
 from twython import Twython, TwythonError
