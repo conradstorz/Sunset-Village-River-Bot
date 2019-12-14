3c2e48d3f8c4cdcb71b6d980222d8f35bd0c03ca
Conrad Storz <conradstorz@gmail.com>
2019-12-14 18:12:10 -0500

    eliminated print() in favor of logging

--- NWS_River_Data_scrape_NEW.py
+++ NWS_River_Data_scrape_NEW.py
@@ -8,7 +8,7 @@ import sys
 from loguru import logger
 
 logger.remove()  # stop any default logger
-LOGGING_LEVEL = "DEBUG"
+LOGGING_LEVEL = "INFO"
 
 from os import sys, path
 from datetime import datetime, timezone
@@ -32,6 +32,7 @@ ACTION_LABELS = ["First-action", "Minor-flood", "Moderate-flood", "Major-flood"]
 MCALPINE_DAM_URL = "https://water.weather.gov/ahps2/hydrograph.php?wfo=iln&gage=mklk2"
 MCALPINE_DAM_NAME = "McAlpine"
 MCALPINE_DAM_DETAILS = {
+    "Friendly_Name": "McAlpine Dam Upper Guage",
     "Dam_URL": MCALPINE_DAM_URL,
     "milemarker": 606.8,
     "guage_elevation": 407.18,
@@ -44,6 +45,7 @@ MCALPINE_DAM_DETAILS = {
 MARKLAND_DAM_URL = "https://water.weather.gov//ahps2/river.php?wfo=lmk&wfoid=18699&riverid=204624&pt%5B%5D=142935&allpoints=150960%2C141893%2C143063%2C144287%2C142160%2C145137%2C143614%2C141268%2C144395%2C143843%2C142481%2C143607%2C145086%2C142497%2C151795%2C152657%2C141266%2C145247%2C143025%2C142896%2C144670%2C145264%2C144035%2C143875%2C143847%2C142264%2C152144%2C143602%2C144126%2C146318%2C141608%2C144451%2C144523%2C144877%2C151578%2C142935%2C142195%2C146116%2C143151%2C142437%2C142855%2C142537%2C142598%2C152963%2C143203%2C143868%2C144676%2C143954%2C143995%2C143371%2C153521%2C153530%2C143683&data%5B%5D=hydrograph"
 MARKLAND_DAM_NAME = "Markland"
 MARKLAND_DAM_DETAILS = {
+    "Friendly_Name": "Markland Dam Lower Guage",
     "Dam_URL": MARKLAND_DAM_URL,
     "milemarker": 531,
     "guage_elevation": 408,
@@ -80,45 +82,51 @@ def current_river_conditions(monitoring_point, dct):
     """ scrape NOAA website for current river conditions.
     Write results to PupDB file and include current flooding action level
     """
+    logger.info(
+        "Scraping webite..."
+        + saferepr(RIVER_MONITORING_POINTS[monitoring_point]["Friendly_Name"])
+    )
     html = retrieve_cleaned_html(RIVER_MONITORING_POINTS[monitoring_point]["Dam_URL"])
-    print('...begin list of "map" objects...')
+    logger.info('...scanning list of "map" objects...')
     map_raw = html.select("map")[0]  # grab first item named 'map'
     parser_engine = ET.XMLParser(recover=True)
     tree = ET.fromstring(str(map_raw), parser=parser_engine)
     root = tree.getroottree()
     root_map = root.getroot()
-    # print(root_map) #logger debug
+    logger.info("map name: " + saferepr(root_map.attrib["name"]))
     map_dict = dct
     for child in root_map:
-        # print('root_map_child tag: ', child.tag)#logger debug
+        logger.debug("root_map_child tag: " + saferepr(child.tag))
         try:
             child_list = child.attrib["alt"].split()
             child_list.append(monitoring_point)
-            # print("=== root_map_child attrib: ", child_list)#logger debug
-            # print(child.attrib["alt"])#logger debug
+            logger.debug(
+                "=== root_map_child 'alt attrib' list: " + saferepr(child_list)
+            )
+            logger.debug("Raw 'attrib' 'alt': " + saferepr(child.attrib["alt"]))
             searchdate = search_dates(child.attrib["title"], languages=["en"])
             if type(searchdate) == list:
                 child_date = searchdate[0][1]  # TODO append dt obj to child_list
                 date_iso = ISO_datestring(
                     child_date, child_list
                 )  # TODO append datestr to child_list
-                # print("search:", type(child_date), date_iso)
+                logger.debug("datestamp search result:" + str(date_iso))
                 if date_iso in map_dict:
-                    logger.error("duplicate key!")  # TODO raise dupkey error
-                    logger.error(child_list)
+                    logger.debug("duplicate key!")  # TODO raise dupkey error
+                    logger.debug(saferepr(child_list))
                 else:
                     observation_key = date_iso + monitoring_point
                     map_dict[observation_key] = child_list
             else:
-                logger.error("no date found")
-                logger.error(child.attrib)
+                logger.debug("no date found")
+                logger.debug(saferepr(child.attrib))
         except ValueError as e:
-            logger.error("no date")
-            logger.error(e)
+            logger.debug("no date")
+            logger.debug(saferepr(e))
         except KeyError:
-            logger.error("no title")
-            logger.error(child.attrib)
-    # pprint(map_dict)
+            logger.debug("no title")
+            logger.debug(saferepr(child.attrib))
+    logger.debug(saferepr(map_dict))
     return map_dict
 
 
@@ -135,7 +143,7 @@ def processRiverData():
     important = ["Forecast:", "Latest", "Highest"]
     for item in times:
         if results[item][0] in important:
-            print(results[item])
+            logger.info(saferepr(results[item]))
     return results  # TODO define a dict of dams and important observations
 
 
@@ -149,6 +157,8 @@ def defineLoggers():
     )
     logger.add(  # create a new log file for each run of the program
         "./LOGS/" + RUNTIME_NAME + "_{time}.log",
+        retention = "10 days",
+        compression = "zip",
         level="DEBUG",  # always send debug output to file
     )
     return
