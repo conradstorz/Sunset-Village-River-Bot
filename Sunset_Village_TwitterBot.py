6fcd8ea683fb83248788dea139d3048763b2da7d
Conrad Storz <conradstorz@gmail.com>
2019-12-30 21:37:00 -0500

    bugs fixed

--- Sunset_Village_TwitterBot.py
+++ Sunset_Village_TwitterBot.py
@@ -20,56 +20,69 @@ from pprint import saferepr
 from pprint import pprint
 
 from pupdb.core import PupDB
+
 PupDB_FILENAME = "SVTB-DB.json_db"
 PupDB_MRTkey = "MostRecentTweet"
 PupDB_MRLkey = "MostRecentRiverLevel"
 PupDB_ACTIONkey = "CurrentFloodingActionLevel"
 
-ACTION_LABELS = ["No Flooding", "First-action", "Minor-flood", "Moderate-flood", "Major-flood"]
-TWEET_FREQUENCY = [18000, 9000, 4000, 2000, 1000] # delay time in seconds
-#ACTION_LEVELS = [21, 23, 30, 38]
-#ACTION_DICT = dict(zip(ACTION_LEVELS, ACTION_LABELS))
+ACTION_LABELS = [
+    "No Flooding",
+    "First-action",
+    "Minor-flood",
+    "Moderate-flood",
+    "Major-flood",
+]
+TWEET_FREQUENCY = [18000, 9000, 4000, 2000, 1000]  # delay time in seconds
+# ACTION_LEVELS = [21, 23, 30, 38]
+# ACTION_DICT = dict(zip(ACTION_LEVELS, ACTION_LABELS))
 LOCATION_OF_INTEREST = 584  # river mile marker @ Bushman's Lake
-OBSERVATION_TAGS = ['Latest', 'Highest']
+OBSERVATION_TAGS = ["Latest", "Highest"]
 
 from twython import Twython, TwythonError
 from TwitterCredentials import APP_KEY
 from TwitterCredentials import APP_SECRET
 from TwitterCredentials import OAUTH_TOKEN
 from TwitterCredentials import OAUTH_TOKEN_SECRET
+
 TWITTER_CREDENTIALS = (APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-#Time between tweets decreases as flooding increases
+# Time between tweets decreases as flooding increases
 MINIMUM_TIME_BETWEEN_TWEETS = TWEET_FREQUENCY[0]
 
 from os import sys, path
+
 RUNTIME_NAME = path.basename(__file__)
 
 from loguru import logger
+
 logger.remove()  # stop any default logger
 LOGGING_LEVEL = "INFO"
-
 
 
 @logger.catch
 def test_tweet():
     data = get_level_data()
     # data contains ALL "imortant" levels
-    #logger.debug(str(data))
-    #TODO create function to extract only 6 most relevent current,highest,eventual levels
+    # logger.debug(str(data))
+    # TODO create function to extract only 6 most relevent current,highest,eventual levels
     return build_tweet(data)
 
 
-'''
+
 @logger.catch
-def send_tweet(tweet, twttr):
+def send_tweet(db, tm, tweet, twttr):
+    """Accept a datetime object, a tweet string and a Twython object. Place tweet and updtae filesystem storage to reflect activities.
+    """
+    # place tweet time into longterm storage
+    db.set(PupDB_MRTkey, str(tm))
     twttr.update_status(status=tweet)
     logger.info("Tweet sent.")
     logger.debug("Tweet string = " + str(tweet))
     logger.info("Length of string = " + str(len(tweet)))
     return True
 
-
+'''
 @logger.catch
 def check_if_time_to_tweet(river_dict, tm, twttr, pdb):
     """ Check if it is time to tweet
@@ -109,6 +122,7 @@ def check_if_time_to_tweet(river_dict, tm, twttr, pdb):
     return waitTime
 '''
 
+
 @logger.catch
 def sanitize(itm):
     """ Observations from NOAA have a format discrepacy between 'Latest' entries
@@ -116,10 +130,10 @@ def sanitize(itm):
     'Standardizes' them.
     """
     sani = itm
-    if itm[0] == 'Latest':
+    if itm[0] == "Latest":
         logger.debug(itm)
         tail = itm[3:]
-        sani = [itm[0], 'Observation:']
+        sani = [itm[0], "Observation:"]
         for i in tail:
             sani.append(i)
         logger.debug(sani)
@@ -136,8 +150,8 @@ def extract_data(map_data, lbl_str):
     observations = {}
     for line in map_data.keys():
         if map_data[line][0] == lbl_str:
-            key = f'{map_data[line][-3]}{map_data[line][-1]}'
-            observations[key] = (sanitize(map_data[line]))
+            key = f"{map_data[line][-3]}{map_data[line][-1]}"
+            observations[key] = sanitize(map_data[line])
     return observations
 
 
@@ -146,10 +160,9 @@ def build_tweet(rivr_conditions_dict):
     """takes a dictionary of river condition observations from 2 dams and builds data into a tweet.
     """
     tweet = " "
-    
-    #TODO put these data gathering functions in seperate functions and return named tuples of results
-    #TODO (damname,observationtype,timestamp,level)
-    #TODO organize data as: currentobservation,highestforecast,eventualforecast)
+    # TODO put these data gathering functions in seperate functions and return named tuples of results
+    # TODO (damname,observationtype,timestamp,level)
+    # TODO organize data as: currentobservation,highestforecast,eventualforecast)
     """
     from collections import namedtuple
 
@@ -159,11 +172,11 @@ def build_tweet(rivr_conditions_dict):
     latest_dict = {}
     for lbl in OBSERVATION_TAGS:
         latest_dict.update(extract_data(rivr_conditions_dict, lbl))
-    #logger.debug(latest_dict)
+    # logger.debug(latest_dict)
     # gather Latest numbers
     for key in latest_dict.keys():
-        logger.debug(f'Entry[{key}]:{str(latest_dict[key])}')
-        if latest_dict[key][0] == 'Latest':
+        logger.debug(f"Entry[{key}]:{str(latest_dict[key])}")
+        if latest_dict[key][0] == "Latest":
             level_obsrvd = float(latest_dict[key][2])
             milemrkr = float(latest_dict[key][-4])
             elevate = float(latest_dict[key][-2])
@@ -177,7 +190,6 @@ def build_tweet(rivr_conditions_dict):
                 dnriver_level = level_obsrvd
                 dnriver_milemrkr = milemrkr
                 dnriver_elevation = elevate
-                obsrv_datetime = latest_dict[key][-1]  
 
     # calculate bushmans level based on latest observation
     slope = upriver_level - dnriver_level
@@ -196,9 +208,9 @@ def build_tweet(rivr_conditions_dict):
     t1 = f"Latest Observation: {upriver_name} {upriver_level}"
     t2 = f" {dnriver_name} {dnriver_level}"
     t3 = f" Calculated Level at Bushmans: {projection:.2f}"
-    tweet = t1 + t2 + t3 + ' ::: Data source: http://portky.com/river.php'
+    tweet = t1 + t2 + t3 + " ::: Data source: http://portky.com/river.php"
     logger.info(tweet)
-    logger.info(f'Length of Tweet {len(tweet)} characters.')
+    logger.info(f"Length of Tweet {len(tweet)} characters.")
     return tweet
 
 
@@ -221,12 +233,8 @@ def UpdatePrediction(twtr, tm, db):
         logger.info("Tweeting...")
         waitTime = 0
         data = get_level_data()
-        sp = build_tweet(data)        
-        # place tweet time into longterm storage
-        db.set(PupDB_MRTkey, str(tm))
-        twtr.update_status(status=sp)
-        logger.info("Tweet sent.")
-        logger.debug("Tweet string = " + str(sp))
+        sp = build_tweet(data)
+        send_tweet(db, tm, sp, twtr)
     else:
         logger.info("Too soon to tweet.")
         waitTime = MINIMUM_TIME_BETWEEN_TWEETS - elapsed.seconds
