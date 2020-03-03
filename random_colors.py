8095401fd823296c434e32e2ae5118d2ac1ed6b6
Conrad Storz <conradstorz@gmail.com>
2020-03-02 22:52:41 -0500

    incr

--- random_colors.py
+++ random_colors.py
@@ -1,8 +1,20 @@
 from itertools import product
 from random import choice
 from time import sleep
+import json
 from sense_hat import SenseHat
 
 sense = SenseHat()
 
 sense.clear(255, 255, 255)
+
+with open("data_file.json", "r") as read_file:
+    color_dict = json.load(read_file)
+
+while True:
+    color = choice(color_dict.keys())
+    print(color)
+    sense.clear(color_dict[color])
+    sleep(1)
+
+    
