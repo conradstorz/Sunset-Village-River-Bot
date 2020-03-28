d7af54f2249f03277d768329853a20322af500ea
Conrad Storz <conradstorz@gmail.com>
2020-03-28 14:26:27 -0400

    typo

--- Sunset_Village_TwitterBot.py
+++ Sunset_Village_TwitterBot.py
@@ -422,7 +422,7 @@ def Main(credentials):
             ) == 0:  # update external displays connected to server each ten seconds.
                 print(".", end="", flush=True)
                 DisplayMessage(
-                    f"  {new_level:.2f}ft Latest. Trend {trend:.2f}ft   Now {new_level:.2f}ft   Trend {trend:.2f}ft"
+                    f"  {new_level:.2f}ft Latest. Trend {trend}ft   Now {new_level:.2f}ft   Trend {trend}ft"
                 )
             if (
                 startDisplay % 50
