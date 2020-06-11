72768dd65734fdb6d337ed0a32c873a8340a197f
Conrad Storz <conradstorz@gmail.com>
2020-06-11 09:15:53 -0400

    folly

---
+++ foo.py/bar.py
@@ -0,0 +1,5 @@
+print(f'This is the python code file bar.py and my name currently is:{__name__}')
+if __name__ == 'bar':
+    print(f'I was imported as a module.')
+print(f'bar.py __file__ variable:{__file__}')    
+print('If the slashes in the file path lean to the right then I am __main__')
---
+++ foo.py/foo.py
@@ -0,0 +1,4 @@
+import bar
+print(f'This is code file foo.py and my name currently is:{__name__}')
+print(f'foo.py __file__ variable:{__file__}')
+print('If the slashes in the file path lean to the right then I am __main__')
