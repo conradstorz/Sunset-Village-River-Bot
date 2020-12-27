969d11abbaba80a76b9b1ac1a6b728d8d28b5670
Conrad Storz <conradstorz@gmail.com>
2020-12-27 18:28:16 -0500

    cleanup

--- filehandling.py
+++ filehandling.py
@@ -68,12 +68,12 @@ def create_timestamp_subdirectory_Structure(timestamp: str):
 """
 
 # set your reference point with the location of the python file you’re writing in
-this_file = Path(__file__)
+# this_file = Path(__file__)
 
 # Here are three ways to get the folder of the current python file
-this_folder1 = Path(__file__, "..")
-this_folder2 = Path(__file__) / ".."
-this_folder3 = Path(__file__).parent
+# this_folder1 = Path(__file__, "..")
+# this_folder2 = Path(__file__) / ".."
+# this_folder3 = Path(__file__).parent
 
 # This will fail:
 # assert this_folder1 == this_folder2 == this_folder3
@@ -90,7 +90,9 @@ this_folder3 = Path(__file__).parent
 # create a new folder:
 # Path("/my/directory").mkdir(parents=True, exist_ok=True)
 
+# find path to your running code
 # project_root = Path(__file__).resolve().parents[1] # this is the folder 2 levels up from your running code.
+
 # create a new PathObj:
 # static_files = project_root / 'static' # if you like this sort of thing they overrode the divide operator.
 # media_files = Path(project_root, 'media') # I prefer this method.
@@ -98,14 +100,17 @@ this_folder3 = Path(__file__).parent
 # how to define 2 sub-directories at once:
 # compiled_js_folder = static_files.joinpath('dist', 'js') # this is robust across all OS.
 
+# gather items from specified path
 # list(static_files.iterdir()) # returns a list of all items in directory.
-
 # [x for x in static_files.iterdir if x.is_dir()] # list of only directories in a folder.
-
 # [x for x in static_files.iterdir if x.is_file()] # same for files only.
 
 # get a list of items matching a pattern:
-# list(compiled_js_folder.glob('*.js')) # returns files ending with '.js'.
+# files = list(compiled_js_folder.glob('*.js')) # returns files ending with '.js'.
+
+# sort the list of files by timestamp:
+# files.sort(key=lambda fn: fn.stat().st_atime)
+# atime, mtime, ctime don't seem to mean what I think. (Pathlib error?)
 
 # search recursively down your folders path:
 # sorted(project_root.rglob('*.js'))
