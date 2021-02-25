a45a200e538f9e42bf3d2887075fc826a7053b57
Conrad Storz <conradstorz@gmail.com>
2021-02-25 09:57:09 -0500

    Added more points of interest

--- OHIO_RIVER_LEVEL_SCRAPING.py
+++ OHIO_RIVER_LEVEL_SCRAPING.py
@@ -23,10 +23,74 @@ from loguru import logger
 
 # this section imports code from the pypi repository (CFSIV-utilities-package) of my own utilities.
 from utils.data2csv import write_csv
-from utils.time_strings import UTC_NOW_STRING, apply_logical_year_value_to_monthday_pair, timefstring, UTC_NOW
+from utils.time_strings import (
+    UTC_NOW_STRING,
+    apply_logical_year_value_to_monthday_pair,
+    timefstring,
+    UTC_NOW,
+)
 from utils.WebScraping import retrieve_cleaned_html
 from utils.filehandling import create_timestamp_subdirectory_Structure
 
+RIVER_GUAGE_IDS = [
+    141893,
+    143063,
+    144287,
+    142160,
+    145137,
+    143614,
+    141268,
+    144395,
+    143843,
+    142481,
+    143607,
+    145086,
+    142497,
+    151795,
+    152657,
+    141266,
+    145247,
+    143025,
+    142896,
+    144670,
+    145264,
+    144035,
+    143875,
+    143847,
+    142264,
+    152144,
+    143602,
+    144126,
+    146318,
+    141608,
+    144451,
+    144523,
+    144877,
+    151578,
+    142935,
+    142195,
+    146116,
+    143151,
+    142437,
+    142855,
+    142537,
+    142598,
+    152963,
+    143203,
+    143868,
+    144676,
+    143954,
+    143995,
+    143371,
+    153521,
+    153530,
+    143683,
+    142935,
+    144523,
+    144877,
+    15157,
+    150960,
+]
 
 # TODO need guage location and elevation data
 mcalpine_upper = "https://water.weather.gov//ahps2/river.php?wfo=lmk&wfoid=18699&riverid=204624&pt%5B%5D=142935&allpoints=150960%2C141893%2C143063%2C144287%2C142160%2C145137%2C143614%2C141268%2C144395%2C143843%2C142481%2C143607%2C145086%2C142497%2C151795%2C152657%2C141266%2C145247%2C143025%2C142896%2C144670%2C145264%2C144035%2C143875%2C143847%2C142264%2C152144%2C143602%2C144126%2C146318%2C141608%2C144451%2C144523%2C144877%2C151578%2C142935%2C142195%2C146116%2C143151%2C142437%2C142855%2C142537%2C142598%2C152963%2C143203%2C143868%2C144676%2C143954%2C143995%2C143371%2C153521%2C153530%2C143683&data%5B%5D=obs&data%5B%5D=xml"
@@ -36,7 +100,13 @@ lsvl_watertower = "https://water.weather.gov//ahps2/river.php?wfo=lmk&wfoid=1869
 cincy = "https://water.weather.gov//ahps2/river.php?wfo=lmk&wfoid=18699&riverid=204624&pt[]=144451&allpoints=150960&data[]=obs&data[]=xml"
 
 
-POINTS_OF_INTEREST = [mcalpine_upper, mrklnd_lower, clifty_creek, lsvl_watertower, cincy]
+POINTS_OF_INTEREST = [
+    mcalpine_upper,
+    mrklnd_lower,
+    clifty_creek,
+    lsvl_watertower,
+    cincy,
+]
 # TODO visualize data from guages to illustrate how a 'hump' of water moves down the river.
 # possibly by graphing the guages as a flat surface and the water elevation above that imagined flat river.
 # TODO predict the time of arrival of the 'hump' at various points. (machine learning?)
@@ -59,16 +129,16 @@ def extract_date(text_list):
         datetime.datetime: Datetime Object
     """
     if type(text_list) != list:
-        raise TypeError('Argument must be a list.')
+        raise TypeError("Argument must be a list.")
 
     for t in text_list:
         found = search_dates(t)
         if found != None:
             for itm in found:
                 s, d = itm
-                if len(s) == 8 and s[2] == ':' == s[5]:
+                if len(s) == 8 and s[2] == ":" == s[5]:
                     return d
-    raise ParserError('No parseable date found.')
+    raise ParserError("No parseable date found.")
 
 
 @logger.catch
@@ -81,19 +151,22 @@ def pull_details(soup):
     guage_id = soup.h1["id"]
     guage_string = soup.h1.string
     # find the comments.
-    comments = soup.findAll(text=lambda text:isinstance(text, Comment))
+    comments = soup.findAll(text=lambda text: isinstance(text, Comment))
     # convert the findAll.ResultSet into a plain list.
     c_list = [c for c in comments]
     # Search the comments for a date (the NWS webscrape contains exactly 1 date/timestamp).
     try:
         scrape_date = extract_date(c_list)
     except ParserError as e:
-        logger.error(f'Could not determine date of scrape from within html text. \nError: {e}')
+        logger.error(
+            f"Could not determine date of scrape from within html text. \nError: {e}"
+        )
         scrape_date = UTC_NOW
-    print(f'Scrape date: {scrape_date}')
+    print(f"Scrape date: {scrape_date}")
     nws_class = soup.find(class_="obs_fores")
     nws_obsfores_contents = nws_class.contents
-    return (nws_obsfores_contents, guage_id, guage_string, scrape_date)   
+    return (nws_obsfores_contents, guage_id, guage_string, scrape_date)
+
 
 @logger.catch
 def get_NWS_web_data(site, cache=False):
@@ -104,6 +177,7 @@ def get_NWS_web_data(site, cache=False):
     clean_soup = retrieve_cleaned_html(site, cache)
     content, id, name, date = pull_details(clean_soup)
     return (content, id, name, date)
+
 
 @logger.catch
 def FixDate(s, scrape_date, time_zone="UTC"):
@@ -118,11 +192,11 @@ def FixDate(s, scrape_date, time_zone="UTC"):
     hours, minutes = time_string.split(":")
     timestamp = datetime.time(int(hours), int(minutes))
     month_digits, day_digits = date_string.split("/")
-    if len(month_digits)+len(day_digits) != 4:
-        raise AssertionError('Month or Day string not correctly extracted.')
+    if len(month_digits) + len(day_digits) != 4:
+        raise AssertionError("Month or Day string not correctly extracted.")
 
     corrected_year = apply_logical_year_value_to_monthday_pair(date_string, scrape_date)
-    
+
     # now place the timestamp back into the date object.
     corrected_datetime = datetime.datetime.combine(corrected_year, timestamp)
 
@@ -136,7 +210,7 @@ def sort_and_label_data(web_data, guage_id, guage_string, scrape_date):
     # the original scrape.
     readings = []
     # Retrieve scrape year so that we can supply it to the processing of the date codes.
-    yyyy = scrape_date.strftime("%Y") # NWS website operates on UTC
+    yyyy = scrape_date.strftime("%Y")  # NWS website operates on UTC
     labels = ["datetime", "level", "flow"]
     for i, item in enumerate(web_data):
         if i >= 1:  # zeroth item is an empty list
@@ -153,17 +227,21 @@ def sort_and_label_data(web_data, guage_id, guage_string, scrape_date):
                 if pointer == 0:  # this is the element for date/time
                     date = FixDate(element, scrape_date)
                     element = timefstring(date)
-                row_dict[labels[pointer]] = element # TODO Add sanity check for this value being a string not an object.
+                row_dict[
+                    labels[pointer]
+                ] = element  # TODO Add sanity check for this value being a string not an object.
                 if pointer == 2:  # end of this reading
                     readings.append(row_dict)  # add to the compilation
                     # reset the dict for next reading
                     row_dict = {"guage": guage_id, "type": sect_name}
     return readings
 
+
 @logger.catch
 def compact_datestring(ds):
     """Return a string representing datetime of provided tuple."""
     return f"{ds[0]}{ds[1]}{ds[2]}_{ds[3]}{ds[4]}"
+
 
 @logger.catch
 def expand_datestring(ds):
@@ -176,11 +254,14 @@ def expand_datestring(ds):
     z = x[1][-3:]
     return (m, d, y, t, z)
 
+
 @logger.catch
 def Main():
     for point in POINTS_OF_INTEREST:
         time_now_string = UTC_NOW_STRING()
-        raw_data, guage_id, friendly_name, scrape_date = get_NWS_web_data(point, cache=True)
+        raw_data, guage_id, friendly_name, scrape_date = get_NWS_web_data(
+            point, cache=True
+        )
         # TODO verify webscraping success
         # DONE, store raw_data for ability to work on dates problem over the newyear transition.
         # It will be helpfull to have 12/28 to  January 4 scrapes for repeated test processing.
@@ -193,10 +274,11 @@ def Main():
             OD = f"{OUTPUT_ROOT}{output_directory}"
             FN = f"{time_now_string}"
             write_csv([item], filename=FN, directory=OD)
-        sleep(1) # guarnatee next point of interest gets a new timestamp.
+        sleep(1)  # guarnatee next point of interest gets a new timestamp.
         # some scrapes process in under 1 second and result in data collision.
         print(time_now_string)
     return True
+
 
 @logger.catch
 def display_cached_data(number_of_scrapes):
@@ -205,8 +287,8 @@ def display_cached_data(number_of_scrapes):
     Args:
         number_of_scrapes (int) : number of scrapes to process from newest towards oldest
     """
-    root = Path(Path.cwd(), 'raw_web_scrapes')
-    files = list(root.glob('*.rawhtml')) # returns files ending with '.rawhtml'
+    root = Path(Path.cwd(), "raw_web_scrapes")
+    files = list(root.glob("*.rawhtml"))  # returns files ending with '.rawhtml'
     # sort the list oldest to newest
     files.sort(key=lambda fn: fn.stat().st_mtime, reverse=True)
     # recover the scrapes
@@ -227,24 +309,23 @@ def display_cached_data(number_of_scrapes):
             data_sample.append(data_list[i])
 
     for point in data_sample[::-1]:
-        datestamp = point['datetime']
+        datestamp = point["datetime"]
         if type(datestamp) == str:
             full_date = datestamp[:10]
         else:
             full_date = datestamp.strftime("%Y/%m/%d")
 
         _dummy = apply_logical_year_value_to_monthday_pair(full_date, scrape_date)
-        print(f'Correct observation date: {_dummy}, original full date: {full_date}')
+        print(f"Correct observation date: {_dummy}, original full date: {full_date}")
     # scrapetime = from filename
     return
-
 
 
 if __name__ == "__main__":
     while True:
         Main()
-        print('Sleeping...')
-        total_sleep = 60*60*6
+        print("Sleeping...")
+        total_sleep = 60 * 60 * 6
         for s in range(total_sleep):
             sleep(1)
             print(f"\r {total_sleep - s}         ", end="", flush=True)
