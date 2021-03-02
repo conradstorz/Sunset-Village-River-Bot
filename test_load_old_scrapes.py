5932bc7858d53e0af55bf6ef790d2ac97e6b46e1
Conrad Storz <conradstorz@gmail.com>
2021-03-01 22:16:08 -0500

    added logging

--- test_load_old_scrapes.py
+++ test_load_old_scrapes.py
@@ -1,3 +1,8 @@
+import sys
+from loguru import logger
 from OHIO_RIVER_LEVEL_SCRAPING import display_cached_data
 
+logger.add(sys.stderr, format="{time} {level} {message}", filter="my_module", level="DEBUG")
+logger.info('Start')
 display_cached_data(9)
+logger.info('End')
